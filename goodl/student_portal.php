9d69233067d9ec3e892276e63c66d6738f05e2d6
Goodluck Alphaxard <masterprogrammerg@gmail.com>
2025-07-18 06:42:29 +0000

    add student_portal practice

--- goodl/student_portal.php
+++ goodl/student_portal.php
@@ -10,80 +10,71 @@
 // export report card as string
 
 // ---Checking for errors
+require_once "./data.php";
 
-
-$students = [
-    [
-        "id"=>1,
-        "fName"=>"Code",
-        "lName"=>"Janja",
-        "subjects"=>[
-            "math"=>90,
-            "apt1020"=>80,
-            "ist2045"=>87
-        ],
-    ],
-    [
-        "id"=>2,
-        "fName"=>"Amina",
-        "lName"=>"Nyongesa",
-        "subjects"=>[
-            "math"=>72,
-            "apt1020"=>85,
-            "ist2045"=>78
-        ],
-    ],
-    [
-        "id"=>3,
-        "fName"=>"Brian",
-        "lName"=>"Otieno",
-        "subjects"=>[
-            "math"=>65,
-            "apt1020"=>70,
-            "ist2045"=>75
-        ],
-    ],
-    [
-        "id"=>4,
-        "fName"=>"Chipo",
-        "lName"=>"Moyo",
-        "subjects"=>[
-            "math"=>88,
-            "apt1020"=>91,
-            "ist2045"=>94
-        ],
-    ],
-    [
-        "id"=>5,
-        "fName"=>"David",
-        "lName"=>"Kariuki",
-        "subjects"=>[
-            "math"=>79,
-            "apt1020"=>68,
-            "ist2045"=>82
-        ],
-    ],
-];
 
 function viewGrade($id,$subj){
+    $subjects = getSubjects($id);
+    foreach($subjects as $name =>$score){
+        if($subj===$name){
+            echo "$name : $score";
+            return;
+        }
+    }
+}
+
+// viewGrade(2,"math");
+function getSubjects($id){
     global $students;
     for($i=0;$i<count($students);$i++){
         $student = $students[$i];
         if($id ===$student["id"]){
-            $subjects = $student["subjects"];
-            foreach($subjects as $name =>$score){
-                if($subj===$name){
-                    echo "$name : $score";
-                    return;
-                }
-            }
-            echo "Subject Not found";
-            return;
-            
+            return $student["subjects"];
         }
     }
-
-    echo "Student Not Found";
 }
 
-viewGrade(2,"math");
+
+function getGPA($id){
+    $subjects = getSubjects($id);
+    $total = 0;
+    foreach($subjects as $subject => $score){
+        $total +=$score;
+    }
+    $gpa = $total/count($subjects);
+    return $gpa;  
+}
+echo "The gpa for student is ".getGPA(1);
+
+
+function addSubject($id,$sub,$score){
+    global $students;
+    for($i=0;$i<count($students);$i++){
+        if($students[$i]['id']==$id){
+           $students[$i]['subjects'][$sub]=$score;    
+        }
+    }
+    return $students;
+}
+
+function removeSubject($id,$sub){
+    global $students;
+    for($i=0;$i<count($students);$i++){
+        if($students[$i]['id']==$id){
+           unset($students[$i]['subjects'][$sub]);    
+        }
+    }
+    return $students;
+}
+
+
+// echo getSubjects(1);
+// echo "added pm and ai <br>";
+// addSubject(1,"pm",40);
+// addSubject(2,"ai",100);
+
+viewGrade(2,'math');
+
+// echo "added  ai <br>";
+// removeSubject(2,'ai');
+// viewGrade(2,'ai');
