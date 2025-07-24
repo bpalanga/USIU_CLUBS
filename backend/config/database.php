b286064d4c443510eb3561c58dad849d4059e810
Goodluck Alphaxard <masterprogrammerg@gmail.com>
2025-07-24 17:33:27 +0000

    Force redeploy to sync DB config

--- backend/config/database.php
+++ backend/config/database.php
@@ -2,7 +2,8 @@ $host = getenv('DB_HOST');
 $db   = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
-
+echo "Connecting to host: $host<br>";
+echo "Using user: $user<br>";
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
