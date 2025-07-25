ec74c5f9bbe3dedc03c8200e74613189d43471e5
alphaxardjr <masterprogrammerg@gmail.com>
2025-07-25 16:34:56 +0300

    fix events

--- backend/app/models/event_model.php
+++ backend/app/models/event_model.php
@@ -9,7 +9,7 @@ function fetch_all_events() {
 
 function save_event($data) {
     global $pdo;
-    $stmt = $pdo->prepare("INSERT INTO events (title, category, description, date, time, location) VALUES (?, ?, ?)");
+    $stmt = $pdo->prepare("INSERT INTO events (title, category, description, date, time, location) VALUES (?, ?, ?, ?, ?, ?)");
     $stmt->execute([$data['title'], data['category'], $data['description'], $data['date'], data['time'], data['location']]);
 }
 
