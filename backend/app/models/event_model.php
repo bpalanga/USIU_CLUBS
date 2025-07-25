68263f8a0871b9e118d33320952ec060bcc7c259
alphaxardjr <masterprogrammerg@gmail.com>
2025-07-25 16:11:01 +0300

    add aditional filed in table

--- backend/app/models/event_model.php
+++ backend/app/models/event_model.php
@@ -9,8 +9,8 @@ function fetch_all_events() {
 
 function save_event($data) {
     global $pdo;
-    $stmt = $pdo->prepare("INSERT INTO events (title, description, date) VALUES (?, ?, ?)");
-    $stmt->execute([$data['title'], $data['description'], $data['date']]);
+    $stmt = $pdo->prepare("INSERT INTO events (title, category, description, date, time, location) VALUES (?, ?, ?)");
+    $stmt->execute([$data['title'], data['category'], $data['description'], $data['date'], data['time'], data['location']]);
 }
 
 function fetch_event_by_id($id) {
@@ -23,7 +23,7 @@ function fetch_event_by_id($id) {
 function update_event($id, $data) {
     global $pdo;
     $stmt = $pdo->prepare("UPDATE events SET title = ?, description = ?, date = ? WHERE id = ?");
-    $stmt->execute([$data['title'], $data['description'], $data['date'], $id]);
+    $stmt->execute([$data['title'], data['category'], $data['description'], $data['date'], data['time'], data['location'],$id]);
 }
 
 function delete_event($id) {
